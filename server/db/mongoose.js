9019511f6967f144483712893924a49713ff4ea8
Biplab <biplabkarki04@gmail.com>
2018-07-10 16:56:15 +0545

    New

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,5 +4,5 @@ mongoose.Promise=global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/GST',{ useNewUrlParser: true });
 //mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
-
+//mongodb://biplab:Meriaama12@ds131971.mlab.com:31971/gst_db
 module.exports={mongoose};
