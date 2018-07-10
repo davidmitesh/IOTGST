11f05ab4c223961bdb04d91e9d4f42bf9a3fbabf
Biplab <biplabkarki04@gmail.com>
2018-07-10 16:45:18 +0545

    New

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
 
-//mongoose.connect('mongodb://localhost:27017/GST',{ useNewUrlParser: true });
-mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/GST',{ useNewUrlParser: true });
+//mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
 
 module.exports={mongoose};
